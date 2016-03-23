44b720e7eff159dd6890e6c477333e75327ff4ed
Andre <andre.m.mendes39@gmail.com>
2016-03-23 18:55:33 +0000

    Add exception

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -17,31 +17,39 @@ public class BrokerPort implements BrokerPortType{
 	public String requestTransport(String origin, String destination, int price)
 			throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
 			UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(origin==null){
+			UnknownLocationFault b = new UnknownLocationFault();
+			throw new UnknownLocationFault_Exception("A origem está errada.", b);}
+		if(price<0){
+			InvalidPriceFault p = new InvalidPriceFault();
+			p.setPrice(price);
+			throw new InvalidPriceFault_Exception("O preço é inválido",p);}
+		/*if(){
+			UnavailableTransportFault u = new UnavailableTransportFault();*/
+			return null;
+		}
+
+		@Override
+		public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
+			if(id==null){
+				UnknownTransportFault b = new UnknownTransportFault();
+				b.setId(id);
+				throw new UnknownTransportFault_Exception("O id é inválido", b);
+			}		return null;
+		}
+
+		@Override
+		public List<TransportView> listTransports() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void clearTransports() {
+			// TODO Auto-generated method stub
+
+		}
+
+		// TODO
+
 	}
-
-	@Override
-	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
-		if(id==null){
-			UnknownTransportFault b = new UnknownTransportFault();
-			b.setId(id);
-			throw new UnknownTransportFault_Exception("O id é inválido", b);
-		}		return null;
-	}
-
-	@Override
-	public List<TransportView> listTransports() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void clearTransports() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	// TODO
-
-}
