c8b9d0f3ad223a3f39a0fad08987609a141acdcc
JBernardino <joao.bernardino@tecnico.ulisboa.pt>
2016-04-11 22:22:16 +0100

    TestesIT do Transporter

--- broker-ws-cli/src/test/java/pt/upa/broker/ws/it/ExampleIT.java
+++ broker-ws-cli/src/test/java/pt/upa/broker/ws/it/ExampleIT.java
@@ -1,7 +1,29 @@
 package pt.upa.broker.ws.it;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.xml.ws.BindingProvider;
+
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.broker.ws.BrokerPortType;
+import pt.upa.broker.ws.BrokerService;
+import pt.upa.broker.ws.InvalidPriceFault_Exception;
+import pt.upa.broker.ws.TransportView;
+import pt.upa.broker.ws.UnavailableTransportFault_Exception;
+import pt.upa.broker.ws.UnavailableTransportPriceFault_Exception;
+import pt.upa.broker.ws.UnknownLocationFault_Exception;
+import pt.upa.broker.ws.UnknownTransportFault_Exception;
+
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 import static org.junit.Assert.*;
+
+import java.util.Map;
+
 
 /**
  *  Integration Test example
@@ -11,43 +33,100 @@ import static org.junit.Assert.*;
  */
 public class ExampleIT {
 
-    // static members
+	// static members
+
+	private static BrokerPortType port;
+
+	// one-time initialization and clean-up
+
+	@BeforeClass
+	public static void oneTimeSetUp() throws Exception{
+
+		String uddiURL = "http://localhost:9090";
+		String name="UpaBroker";
+
+		UDDINaming uddiNaming = new UDDINaming(uddiURL);
+		String endpointAddress = uddiNaming.lookup(name);
+
+		BrokerService service = new BrokerService();
+		port = service.getBrokerPort();
+
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+		port = null;
+
+	}
 
 
-    // one-time initialization and clean-up
-
-    @BeforeClass
-    public static void oneTimeSetUp() {
-
-    }
-
-    @AfterClass
-    public static void oneTimeTearDown() {
-
-    }
+	// members
 
 
-    // members
+	// initialization and clean-up for each test
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
 
 
-    // initialization and clean-up for each test
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
+	// tests
 
 
-    // tests
 
-    @Test
-    public void test() {
+	@Test
+	public void test() {
+		
+		String actual = "";
+		String expected = "Just Pinged test";
+		
+		actual =  port.ping("test");
+		
+		assertEquals(expected, actual);
+		// if the assert fails, the test fails
+	}
 
-        // assertEquals(expected, actual);
-        // if the assert fails, the test fails
-    }
+	/*
+	@Test
+	public void test01() {
 
+		String actual = "";
+		String expected = "A origem está errada.";
+
+		try {
+			actual =  port.requestTransport("Barcelona", "Leiria", 5);
+		} catch (InvalidPriceFault_Exception | UnavailableTransportFault_Exception
+				| UnavailableTransportPriceFault_Exception | UnknownLocationFault_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(expected, actual);
+	}*/
+	
+	/*
+	@Test
+	public void test02() {
+		
+		TransportView actual
+		Class<UnknownTransportFault_Exception> expected = UnknownTransportFault_Exception.class;
+	
+		try {
+			actual = port.viewTransport(null);
+		} catch (UnknownTransportFault_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		assertEquals(expected, actual);
+	}
+	*/
 }
--- transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/ExampleIT.java
+++ transporter-ws-cli/src/test/java/pt/upa/transporter/ws/it/ExampleIT.java
@@ -2,6 +2,7 @@ package pt.upa.transporter.ws.it;
 
 import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.Map;
 
@@ -66,75 +67,155 @@ public class ExampleIT {
 
 	@Before
 	public void setUp() {
+		port.clearJobs();
 	}
 
 	@After
 	public void tearDown() {
-	}}
+	}
 
 
 	// tests
 
-//	@Test
-//	public void test01() {
-//
-//		String actual = "";
-//		String expected = "UpaTransporter1";
-//
-//		try {
-//			actual =  port.requestJob("Lisboa", "Leiria", 5).getCompanyName();
-//		} catch (BadLocationFault_Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (BadPriceFault_Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		assertEquals(expected, actual);
-//		// if the assert fails, the test fails
-//	}
-//
-//	@Test
-//	public void test02() {
-//		JobStateView actual = JobStateView.values()[0];
-//		JobStateView expected = JobStateView.values()[1];
-//
-//		try {
-//			port.decideJob("1", false);
-//			actual = port.jobStatus("1").getJobState();
-//		} catch (BadJobFault_Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		assertEquals(expected, actual);
-//	}
-//
-//	@Test
-//	public void test03() {
-//		JobStateView actual = JobStateView.values()[0];
-//		JobStateView expected = JobStateView.values()[2];
-//
-//		try {
-//			port.decideJob("1", true);
-//			actual = port.jobStatus("1").getJobState();
-//		} catch (BadJobFault_Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		assertEquals(expected, actual);
-//	}
-//
-//	@Test
-//	public void test04() {
-//		JobView actual;
-//		JobView expected = null;
-//
-//		actual=port.jobStatus("20");
-//
-//		assertEquals(expected, actual);
-//	}
-//	
-//	
-//}
+
+	//Testes requestJob
+
+	@Test(expected = BadLocationFault_Exception.class)  
+	public void badLocationOrigemRequestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+		port.requestJob("Paris", "Porto", 10);
+	}
+
+	@Test(expected = BadLocationFault_Exception.class)  
+	public void badLocationDestinoRequestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+		port.requestJob("Lisboa", "Barcelona", 10);
+	}
+
+	@Test(expected = BadPriceFault_Exception.class)  
+	public void badPriceFaultRequestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+		port.requestJob("Lisboa", "Porto", -5);
+	}
+
+
+
+	@Test
+	public void priceHighRequestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+
+		JobView actual = port.requestJob("Lisboa", "Porto", 101);
+
+		assertNull(actual);
+
+	}
+	
+	
+	/* Teste da proposta de job entre os 10 e 100 euros
+	@Test
+	public void priceJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+
+		JobView actual = port.requestJob("Lisboa", "Porto", 50);
+
+		assertNull(actual);
+
+	}*/
+
+	@Test
+	public void priceLowResquestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+		JobView actual = port.requestJob("Lisboa", "Porto", 4);
+		assertEquals("Lisboa", actual.getJobOrigin());
+		assertEquals("Porto", actual.getJobDestination());
+		//assertEquals("1", actual.getJobIdentifier());
+		//assertEquals(4, actual.getJobPrice());
+		assertEquals(JobStateView.values()[0], actual.getJobState());
+
+	}
+
+	@Test
+	public void upaTransporter1RequestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+		String expected = "UpaTransporter1";
+
+		String actual =  port.requestJob("Lisboa", "Beja", 5).getCompanyName();
+
+		assertEquals(expected, actual);
+		// if the assert fails, the test fails
+	}
+	
+	@Test
+	public void upaTransporter2RequestJobTest() throws BadLocationFault_Exception, BadPriceFault_Exception {
+
+		String expected = "UpaTransporter2";
+
+		String actual =  port.requestJob("Leiria", "Braga", 5).getCompanyName();
+
+		assertEquals(expected, actual);
+		// if the assert fails, the test fails
+	}
+
+	//Testes decideJob
+
+	@Test(expected=BadJobFault_Exception.class)
+	public void badJobFaultDecideJobTest() throws BadJobFault_Exception, BadLocationFault_Exception, BadPriceFault_Exception {
+		
+		port.requestJob("Lisboa", "Porto", 55);
+		
+		port.decideJob("100", false);
+
+	}
+	
+	@Test
+	public void acceptedDecideJobTest() throws BadJobFault_Exception, BadLocationFault_Exception, BadPriceFault_Exception{
+		
+		port.requestJob("Lisboa", "Porto", 5);
+
+		JobStateView expected = JobStateView.values()[2];
+		JobStateView actual = port.decideJob("1", true).getJobState();
+		
+		assertEquals(expected, actual);
+		
+	}
+		
+	@Test
+	public void rejectedDecideJobTest() throws BadJobFault_Exception, BadLocationFault_Exception, BadPriceFault_Exception{
+		
+		port.requestJob("Lisboa", "Porto", 5);
+
+		JobStateView expected = JobStateView.values()[1];
+		JobStateView actual = port.decideJob("1", false).getJobState();
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	//Testes jobStatus
+	
+	@Test
+	public void nullJobStatusTest(){
+		
+		JobView actual = port.jobStatus("1234");
+		
+		assertNull(actual);
+	}
+	
+	@Test
+	public void listJobStatusTest() throws BadJobFault_Exception, BadLocationFault_Exception, BadPriceFault_Exception{
+		
+		port.requestJob("Lisboa", "Porto", 5);
+		JobView actual = port.jobStatus("1");
+		
+		
+		//jobs.put("1", new Job("Lisboa", "Porto", "UpaTransporter1", "1", 10, JobStateView.values()[0]));
+		
+		
+		assertEquals("UpaTransporter2", actual.getCompanyName());
+		assertEquals("1", actual.getJobIdentifier());
+		assertEquals("Lisboa", actual.getJobOrigin());
+		assertEquals("Porto", actual.getJobDestination());
+		assertEquals(JobStateView.values()[0], actual.getJobState()); /// verificar depois de fazer a limpeza
+		
+	}	
+}
