ac81e92ceb7b858ac75e0cfbe0bb8137b0cee833
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2016-05-11 18:49:13 +0100

    Update broker client

--- broker-ws-cli/src/main/java/pt/upa/broker/BrokerClientApplication.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/BrokerClientApplication.java
@@ -1,68 +1,17 @@
 package pt.upa.broker;
 
-import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
-
-import java.util.Map;
-
-import javax.xml.ws.BindingProvider;
-
-import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
-import pt.upa.broker.ws.BrokerPortType;
-import pt.upa.broker.ws.BrokerService;
+import pt.upa.broker.ws.cli.BrokerClient;
 
 public class BrokerClientApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(BrokerClientApplication.class.getSimpleName() + " starting...");
 
-		// Check arguments
-		if (args.length < 2) {
-			System.err.println("Argument(s) missing!");
-			System.err.printf("Usage: java %s uddiURL name%n", BrokerClientApplication.class.getName());
-			return;
-		}
-
 		String uddiURL = args[0];
 		String name = args[1];
+
+		System.out.println(uddiURL+name);
+		BrokerClient client = new BrokerClient(uddiURL, name);
 		
-		
-//		String uddiURL = "http://localhost:9090";
-//		String name="UpaBroker3";
-		
-
-		System.out.printf("Contacting UDDI at %s%n", uddiURL);
-		UDDINaming uddiNaming = new UDDINaming(uddiURL);
-
-		System.out.printf("Looking for '%s'%n", name);
-		String endpointAddress = uddiNaming.lookup(name);
-
-		if (endpointAddress == null) {
-			System.out.println("Not found!");
-			return;
-		} else {
-			System.out.printf("Found %s%n", endpointAddress);
-		}
-
-		System.out.println("Creating stub ...");
-		BrokerService service = new BrokerService();
-		BrokerPortType port = service.getBrokerPort();
-
-		System.out.println("Setting endpoint address ...");
-		BindingProvider bindingProvider = (BindingProvider) port;
-		Map<String, Object> requestContext = bindingProvider.getRequestContext();
-		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
-
-		System.out.println("Remote call ...");
-		
-//		String a =port.requestTransport("Porto", "Braga", 10);
-		
-		System.out.println(port.listTransports());
-//		port.clearTransports();
-//		System.out.println(y);
-//		//System.out.println(port.viewTransport("1").getState());
-		String y="1";
-		System.out.println(port.viewTransport(y).getOrigin()+port.viewTransport(y).getDestination() + port.viewTransport(y).getPrice()+ port.viewTransport(y).getState()+port.viewTransport(y).getTransporterCompany());
-
 	}
 }
-
--- broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClient.java
+++ broker-ws-cli/src/main/java/pt/upa/broker/ws/cli/BrokerClient.java
@@ -1,7 +1,98 @@
 package pt.upa.broker.ws.cli;
 
-public class BrokerClient {
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
-	// TODO
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.registry.JAXRException;
+import javax.xml.ws.BindingProvider;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.upa.broker.ws.BrokerPortType;
+import pt.upa.broker.ws.BrokerService;
+import pt.upa.broker.ws.InvalidPriceFault_Exception;
+import pt.upa.broker.ws.TransportView;
+import pt.upa.broker.ws.UnavailableTransportFault_Exception;
+import pt.upa.broker.ws.UnavailableTransportPriceFault_Exception;
+import pt.upa.broker.ws.UnknownLocationFault_Exception;
+import pt.upa.broker.ws.UnknownTransportFault_Exception;
+
+public class BrokerClient implements BrokerPortType{
+
+	String uddiURL = "";
+	String name="";
+	// service, port as members
+	BrokerPortType stub;
+	BrokerService service;
+
+	// constructor
+	public BrokerClient(String uddiURL, String name) throws JAXRException {
+		this.uddiURL=uddiURL;
+		this.name=name;
+
+		System.out.printf("Contacting UDDI at %s%n", uddiURL);
+		UDDINaming uddiNaming = new UDDINaming(uddiURL);
+
+		System.out.printf("Looking for '%s'%n", name);
+		String endpointAddress = uddiNaming.lookup(name);
+
+		if (endpointAddress == null) {
+			System.out.println("Not found!");
+			throw new IllegalArgumentException("Não encontrei no uddi");
+		} else {
+			System.out.printf("Found %s%n", endpointAddress);
+		}
+
+		System.out.println("Creating stub ...");
+		service = new BrokerService();
+		stub = service.getBrokerPort();
+
+		System.out.println("Setting endpoint address ...");
+		BindingProvider bindingProvider = (BindingProvider) stub;
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+
+	}
+
+
+
+	@Override
+	public List<TransportView> update(TransportView info) {
+		return stub.update(info);
+	}
+
+	@Override
+	public int imAlive() {
+		return stub.imAlive();
+	}
+
+	@Override
+	public String ping(String name) {
+		return stub.ping(name);
+	}
+
+	@Override
+	public String requestTransport(String origin, String destination, int price)
+			throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception,
+			UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+		return stub.requestTransport(origin, destination, price);
+	}
+
+	@Override
+	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception {
+		return stub.viewTransport(id);
+	}
+
+	@Override
+	public List<TransportView> listTransports() {
+		return stub.listTransports();
+	}
+
+	@Override
+	public void clearTransports() {
+		stub.clearTransports();
+	}
+
 
 }
